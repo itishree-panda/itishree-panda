b601c2580a58b9ba1375b5b28845d63304b4c2c2
Nirmala Kumar Sahu <itishreepanda.1998@gmail.com>
2026-02-25 21:47:59 +0530

    added new project

---
+++ src/components/Footer.jsx
@@ -0,0 +1,54 @@
+export default function Footer() {
+  return (
+    <div className="mt-20">
+      <div className="text-center">
+        <a href="#!">
+          <img
+            src="/assets/logo.png"
+            alt=""
+            className="w-36 mx-auto mb-2 dark:hidden"
+          />
+          <img
+            src="/assets/logo_dark.png"
+            alt=""
+            className="w-36 mx-auto mb-2 hidden dark:block"
+          />
+        </a>
+
+        <div className="w-max flex items-center gap-2 mx-auto">
+          <img
+            src="./assets/mail_icon.png"
+            alt=""
+            className="w-5 dark:hidden"
+          />
+          <img
+            src="./assets/mail_icon_dark.png"
+            alt=""
+            className="w-5 hidden dark:block"
+          />
+
+          <a href="mailto:itishreepanda.1998@gmail.com">
+            itishreepanda.1998@gmail.com
+          </a>
+        </div>
+      </div>
+      <div className="text-center sm:flex items-center justify-between border-t border-gray-400 mx-[10%] mt-12 py-3">
+        <p>
+          © {new Date().getFullYear()} Itishree Panda • All rights reserved.
+        </p>
+        <ul className="flex items-center gap-10 justify-center mt-4 sm:mt-0">
+          <li>
+            <a href="https://github.com/itishree-panda" target="_blank">
+              GitHub
+            </a>
+          </li>
+          <li>
+            <a href="https://www.linkedin.com/in/itishreep/" target="_blank">
+              LinkedIn
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,65 @@
+export default function Header() {
+  return (
+    <div className="w-11/12 max-w-3xl text-center mx-auto h-screen flex flex-col items-center justify-center gap-4">
+      <img
+        src="./assets/profile/profile-img.png"
+        alt=""
+        className="rounded-full w-32"
+      />
+      <h3 className="flex items-end gap-2 text-xl md:text-3xl mb-3 font-Ovo">
+        Hi! I&apos;m Itishree Panda
+        <img src="./assets/hand-icon.png" alt="" className="w-6 mb-1" />
+      </h3>
+      <h1 className="text-3xl sm:text-6xl lg:text-[60px] font-Ovo">
+        Full Stack MERN Stack Developer
+      </h1>
+      <p className="max-w-2xl mx-auto font-Ovo">
+        I am a Full Stack MERN Developer, with
+        {(() => {
+          const start = new Date(2022, 9); // Sept 2022
+          const now = new Date();
+          const totalMonths =
+            (now.getFullYear() - start.getFullYear()) * 12 +
+            (now.getMonth() - start.getMonth());
+
+          // if want to subtract gap
+          const gap = 3; // 3 months gap
+          const adjustedMonths = totalMonths - gap;
+          const years = Math.floor(adjustedMonths / 12);
+          const months = adjustedMonths % 12;
+
+          return ` ${years}.${months} `;
+        })()}
+        years of experience in multiple companies like M2P Fintech, Finxbridge
+        and Ratnam.
+      </p>
+
+      <div className="flex flex-col sm:flex-row items-center gap-4 mt-4">
+        <a
+          href="#contact"
+          className="px-10 py-2.5 border rounded-full bg-gradient-to-r from-[#b820e6] to-[#da7d20] text-white flex items-center gap-2 dark:border-transparent"
+        >
+          contact me{" "}
+          <img
+            src="./assets/profile/right-arrow-white.png"
+            alt=""
+            className="w-4"
+          />
+        </a>
+
+        <a
+          href="./assets/documents/ITISHREE_PANDA_RESUME.pdf"
+          download
+          className="px-10 py-2.5 rounded-full border border-gray-300 dark:border-white/25 hover:bg-slate-100/70 dark:hover:bg-darkHover flex items-center gap-2 bg-white dark:bg-transparent dark:text-white"
+        >
+          my resume{" "}
+          <img
+            src="./assets/profile/download-icon.png"
+            alt=""
+            className="w-4 dark:invert"
+          />
+        </a>
+      </div>
+    </div>
+  );
+}
